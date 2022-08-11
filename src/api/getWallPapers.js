71c78059c970350a3513cc9de03fff7600968dd1
kimjiwon506 <kimjiwon506@gmail.com>
2022-08-11 12:40:49 +0900

    URLSearchParams 사용해서 encoded하기

--- src/api/getWallPapers.js
+++ src/api/getWallPapers.js
@@ -2,10 +2,13 @@ import request from './request';
 
 const BASE_URL = 'https://pixabay.com/api';
 
+const defaultParams = {
+    key: process.env.REACT_APP_PIXABAY,
+};
+
 const getWallPapers = async () => {
-    const result = await request(
-        `${BASE_URL}/?key=${process.env.REACT_APP_PIXABAY}`
-    );
+    const params = URLSearchParams(defaultParams).toString();
+    const result = await request(`${BASE_URL}/?${params}`);
     return result;
 };
 
